50d3a21321e1406d66d6c94c155db6bd90b295e8
lakshmantgld <diwaakartg@gmail.com>
2018-03-21 11:41:03 +0900

    ✨ Feat: Validation firebase ID token
    validates each request with the attached firebase ID token, thus protecting the cloud functions

---
+++ index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var admin = require('firebase-admin');
+var serviceAccount = require('./serviceAccount.json');
+
+// Initialzing the firebase with the appropriate service Account.  
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+// Validating the firebase ID token attached with each request.
+const validateFirebaseIdToken = (request) =>
+new Promise((resolve, reject) => {
+  console.log('====================================');
+  console.log('Headers:' +  JSON.stringify(request.headers));
+  console.log('====================================');
+
+  let idToken;
+  if (request.headers.authorization && request.headers.authorization.startsWith('Bearer ')) {
+    console.log('Found "Authorization"  header');
+    // Read the ID Token from the Authorization header.
+    idToken = request.headers.authorization.split('Bearer ')[1];
+  } else {
+    console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
+        'Make sure you authorize your request by providing the following HTTP header:',
+        'Authorization: Bearer <Firebase ID Token>',
+        'or by passing a "__session" cookie.');
+    reject("unAuthorized");
+  }
+
+  admin.auth().verifyIdToken(idToken).then((decodedIdToken) => {
+    console.log('ID Token correctly decoded', decodedIdToken);
+    resolve(idToken);
+  }).catch((error) => {
+    console.error('Error while verifying Firebase ID token:', error);
+    reject("unAuthorized");
+  });
+});
+
+
+exports.protected = (request, response) => {
+  validateFirebaseIdToken(request)
+  .then((token) => {
+    console.log('====================================');
+    console.log('token: ' + token);
+    console.log('====================================');
+    response.set('Access-Control-Allow-Origin', '*');
+    response.status(200).send("Hello from protected functions");
+  })
+  .catch((error) => {
+    console.error('In Main Catch - error: ' + error);
+    response.set('Access-Control-Allow-Origin', '*');
+    if (error === "unAuthorized") {
+      response.status(403).send("UnAuthorized - Make sure you add your token to Authorization header");
+    } else {
+      response.status(500).send(error);
+    }
+  });
+};
